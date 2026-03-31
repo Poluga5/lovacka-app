3ff4a30df8b8088c71987b32eb9226796389a5b8
Poluga5 <bockorsasa@gmail.com>
2026-03-31 23:12:01 +0200

    Update client.ts

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -2,7 +2,7 @@ import { createBrowserClient } from '@supabase/ssr'
 
 export function createClient() {
   return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    'https://dlcehyicarwwbjkluofl.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsY2VoeWljYXJ3d2Jqa2x1b2ZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5MDE0NjAsImV4cCI6MjA5MDQ3NzQ2MH0.HhlPHRjUm6bski9GLrCXZkm0Dw7LF8kfPjJbw68WrFg'
   )
 }
